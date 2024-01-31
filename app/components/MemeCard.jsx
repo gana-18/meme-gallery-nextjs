3085ad11a9b48bc72c5c4b6312c519e044f4b378
Gana Sai Teja <velichetiganasaiteja@gmail.com>
2024-01-31 15:24:45 +0530

    cache,comments

--- app/components/MemeCard.jsx
+++ app/components/MemeCard.jsx
@@ -6,15 +6,15 @@ import { Gallery, Item } from 'react-photoswipe-gallery'
 import LoadingPage from '../loading';
 
 function MemeCard() {
-  const [memes, setMemes] = useState([]);
-    const [loading, setLoading] = useState(true);
+    const [memes, setMemes] = useState([]);
+    const [loading, setLoading] = useState(true); // Keep track of when the memes are being fetched
     const [error, setError] = useState(null);
     const [after, setAfter] = useState(null); // Keep track of the 'after' attribute for pagination
     const isMounted = useRef(true);
 
     const fetchMemes = async () => {
         try {
-            const response = await fetch(`https://www.reddit.com/r/memes.json?limit=100${after ? `&after=${after}` : ''}`);
+            const response = await fetch(`https://www.reddit.com/r/memes.json?limit=100${after ? `&after=${after}` : ''}`,{cache: "force-cache"});
             const json = await response.json();
             setMemes((prevMemes) => [...prevMemes, ...json.data.children]);
             setAfter(json.data.after);
@@ -27,12 +27,12 @@ function MemeCard() {
 
     useEffect(() => {
          if (isMounted.current) {
-           fetchMemes();
+           fetchMemes();                   // fetch memes only once when the component is mounted
           isMounted.current = false;
          }
     }, []);
 
-    const handleScroll = () => {
+    const handleScroll = () => {         // infinite scrolling for every batch of memes
         if (
             window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight &&
             !loading
@@ -46,6 +46,9 @@ function MemeCard() {
             window.removeEventListener('scroll', handleScroll);
         };
     }, [loading]);
+
+    if (error) return <div className='error'>{error}</div>;   // error handling
+
   return (
       <>
       {loading?(
@@ -104,74 +107,3 @@ function MemeCard() {
 
 
 export default MemeCard;
-
-
-/* function MemeCard(){
-    const [memes, setMemes] = useState([]);
-    const [loading,setLoading]=useState(true)
-
-    const fetchMemes = async () => {
-        const response = await fetch('https://www.reddit.com/r/memes.json?limit=1000');
-        const json = await response.json();
-        setMemes(json.data.children);
-        setLoading(false)
-    }
-    useEffect(() => {
-        fetchMemes();
-    }, []);
-    memes?memes.shift():console.log("Loading");
-    return (
-        <>
-        {loading?(
-            <LoadingPage/>
-        ):
-        <Gallery>
-            <div className='items'>
-            {memes.map((meme)=>(
-                 <Item
-                   original={meme.data.is_video?meme.data.secure_media.reddit_video.fallback_url:meme.data.url}
-                   thumbnail={meme.data.thumbnail==="nsfw"?meme.data.url:meme.data.thumbnail}
-                   width="900"
-                   height="700"
-                >
-                 {({ ref, open }) => (
-                    <div className='elements'>
-                        {meme.data.is_video?(
-                            <video ref={ref} onClick={open} controls
-                             priority={true}
-                            >
-                             <source src={meme.data.secure_media.reddit_video.fallback_url} type="video/mp4" />
-                             Your browser does not support the video tag.
-                           </video>
-                         ):(meme.data.url.endsWith('.gif')?(
-                            <img 
-                              ref={ref} 
-                              onClick={open} 
-                              src={meme.data.url} 
-                              alt={"Gif Memes"} 
-                              loading='lazy'
-                            />
-                         ):(
-                            <Image
-                              ref={ref} 
-                              onClick={open}
-                              src={meme.data.url}
-                              alt={meme.data.title}
-                              fill
-                              loading='eager'
-                        />
-                        ))}
-                    </div>
-                  
-                 )}
-                </Item>
-            ))}
-            </div>
-       </Gallery>
-        }
-        
-       </>
-    );
-}
-
-export default MemeCard*/
